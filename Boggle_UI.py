43cda9fdc2e785abe047040770a13bcfbbad70f0
CaptainSkyVenom <zhiwei_gan@hotmail.com>
2017-12-15 13:41:14 -0500

    Boggle_UI Game Intro screen added

--- Boggle_UI.py
+++ Boggle_UI.py
@@ -1,2 +1,73 @@
-class UI:
-	
+import pygame
+import time
+import random
+
+pygame.init()
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+bright_green = (100,255,100)
+bright_red = (255,100,100)
+
+block_color = (53,115,255)
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Boggle')
+clock = pygame.time.Clock()
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
+def quitGame():
+    pygame.quit()
+    quit()
+
+def test():
+    print("clicked")
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.SysFont("comicsansms",115)
+        TextSurf, TextRect = text_objects("Boggle!", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("GO!",150,450,100,50,green,bright_green,test)
+        button("Quit",550,450,100,50,red,bright_red,quitGame)
+
+        pygame.display.update()
+        clock.tick(15)
+
+game_intro()
+
