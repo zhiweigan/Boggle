b713be1dd9c2e9a6dd80ff54d9e4c23e6f1576b7
cxiao18 <cxiao18@choate.edu>
2017-12-15 13:45:32 -0500

    print board
    works but its VERY inefficient

--- Boggle_Grid.py
+++ Boggle_Grid.py
@@ -13,39 +13,33 @@ import random
 class Grid:
 
 	def __init__(self):
-		self.grid = []
-		self.board = ''
-	
+		self.line1 = ''
+		self.line2 = ''
+		self.line3 = ''
+		self.line4 = ''
+
 	def makeBoard(self):
-		letters = {0: 'A',
-				1: 'B',
-				2: 'C', 
-				3: 'D', 
-				4: 'E', 
-				5: 'F', 
-				6: 'G', 
-				7: 'H', 
-				8: 'I', 
-				9: 'J', 
-				10: 'K',
-				11: 'L', 
-				12: 'M', 
-				13: 'N', 
-				14: 'O', 
-				15: 'P', 
-				16: 'Q', 
-				17: 'R',
-				18: 'S', 
-				19: 'T', 
-				20: 'U', 
-				21: 'V', 
-				22: 'W', 
-				23: 'X', 
-				24: 'Y', 
-				25: 'Z'}
-		for i in range(16):
-			self.board += (letters[random.randint(0,25)])
-		print(self.board)
+		die0 = ['R', 'I', 'F', 'O', 'B', 'X']
+		die1 = ['I', 'F', 'E', 'H', 'E', 'Y']
+		die2 = ['D', 'E', 'N', 'O', 'W', 'S']
+		die3 = ['U', 'T', 'O', 'K', 'N', 'D']
+		die4 = ['H', 'M', 'S', 'R', 'A', 'O']
+		die5 = ['L', 'U', 'P', 'E', 'T', 'S']
+		die6 = ['A', 'C', 'I', 'T', 'O', 'A']
+		die7 = ['Y', 'L', 'G', 'K', 'U', 'E']
+		die8 = ['Qu', 'B', 'M', 'J', 'O', 'A']
+		die9 = ['E', 'H', 'I', 'S', 'P', 'N']
+		die10 = ['V', 'E', 'T', 'I', 'G', 'N']
+		die11 = ['B', 'A', 'L', 'I', 'Y', 'T']
+		die12 = ['E', 'Z', 'A', 'V', 'N', 'D']
+		die13 = ['R', 'A', 'L', 'E', 'S', 'C']
+		die14 = ['U', 'W', 'I', 'L', 'R', 'G']
+		die15 = ['P', 'A', 'C', 'E', 'M', 'D']
+
+		print(die0[random.randint(0,5)], die1[random.randint(0,5)], die2[random.randint(0,5)], die3[random.randint(0,5)])
+		print(die4[random.randint(0,5)], die5[random.randint(0,5)], die6[random.randint(0,5)], die7[random.randint(0,5)])
+		print(die8[random.randint(0,5)], die9[random.randint(0,5)], die10[random.randint(0,5)], die11[random.randint(0,5)])
+		print(die12[random.randint(0,5)], die13[random.randint(0,5)], die14[random.randint(0,5)], die15[random.randint(0,5)])
 
 	#def wordCheck(self):
 		#json file used here
